861ec01c863b9dec4737eca0a98dfabce951d74b
Anton Drobyshev <antondrob@bk.ru>
2021-07-28 22:02:47 +0300

    First review attempt

--- src/components/burger-ingredients/BurgerIngredients.js
+++ src/components/burger-ingredients/BurgerIngredients.js
@@ -8,6 +8,8 @@ import {
     Box
 } from '@ya.praktikum/react-developer-burger-ui-components';
 import BurgerIngredientsStyles from './BurgerIngredientsStyles.module.css';
+import Modal from '../modal/Modal';
+import IngredientDetails from '../ingredient-details/IngredientDetails';
 
 function BurgerIngredients(props) {
     const [state, setState] = React.useState({
@@ -32,8 +34,47 @@ function BurgerIngredients(props) {
             "60666c42cc7b410027a1a9b1": 1,
             "60666c42cc7b410027a1a9b8": 1,
             "60666c42cc7b410027a1a9b6": 2
+        },
+        modal: {
+            isOpened: false,
+            data: null
         }
     });
+
+    const closeIngredient = React.useCallback(
+        () => {
+            setState({
+                ...state,
+                modal: {
+                    isOpened: false,
+                    data: null
+                }
+            })
+        },
+        [state]
+    );
+
+    React.useEffect(() => {
+        const handleEsc = (event) => {
+            if (event.keyCode === 27 && state.modal.isOpened) {
+                closeIngredient();
+            }
+        };
+        document.addEventListener('keydown', handleEsc);
+        return () => {
+            document.removeEventListener('keydown', handleEsc);
+        }
+    }, [state.modal.isOpened, closeIngredient]);
+
+    const openIngredient = (el) => {
+        setState({
+            ...state,
+            modal: {
+                isOpened: true,
+                data: el
+            }
+        });
+    }
 
     return (
         <section className={BurgerIngredientsStyles.leftSidebar}>
@@ -53,7 +94,7 @@ function BurgerIngredients(props) {
                         <ul className={BurgerIngredientsStyles.list}>
                             {props.products.filter(el => el.type === 'bun').map((el, index) => {
                                 return (
-                                    <li key={el._id}>
+                                    <li key={el._id} onClick={(e) => openIngredient(el)}>
                                         {el._id in state.addedProducts &&
                                         <Counter count={state.addedProducts[el._id]} size="default"/>}
                                         <div className={BurgerIngredientsStyles.listItem}>
@@ -74,7 +115,7 @@ function BurgerIngredients(props) {
                         <ul className={BurgerIngredientsStyles.list}>
                             {props.products.filter(el => el.type === 'sauce').map((el, index) => {
                                 return (
-                                    <li key={el._id}>
+                                    <li key={el._id} onClick={(e) => openIngredient(el)}>
                                         {el._id in state.addedProducts &&
                                         <Counter count={state.addedProducts[el._id]} size="default"/>}
                                         <div className={BurgerIngredientsStyles.listItem}>
@@ -95,7 +136,7 @@ function BurgerIngredients(props) {
                         <ul className={BurgerIngredientsStyles.list}>
                             {props.products.filter(el => el.type === 'main').map((el, index) => {
                                 return (
-                                    <li key={el._id}>
+                                    <li key={el._id} onClick={(e) => openIngredient(el)}>
                                         {el._id in state.addedProducts &&
                                         <Counter count={state.addedProducts[el._id]} size="default"/>}
                                         <div className={BurgerIngredientsStyles.listItem}>
@@ -113,6 +154,11 @@ function BurgerIngredients(props) {
                     </div>
                 </div>
             </div>
+            {state.modal.isOpened && (
+                <Modal header="Детали ингредиента" onClose={closeIngredient}>
+                    <IngredientDetails details={state.modal.data}/>
+                </Modal>
+            )}
         </section>
     )
 }
---
+++ src/components/modal-overlay/ModalOverlay.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ModalOverlayStyles from './ModalOverlayStyles.module.css';
+import PropTypes from "prop-types";
+
+function ModalOverlay({onClose}) {
+    return <div onClick={onClose} className={ModalOverlayStyles.overlay}></div>
+}
+
+ModalOverlay.propTypes = {
+    onClose: PropTypes.func.isRequired
+};
+
+export default ModalOverlay;
