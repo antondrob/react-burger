1724dff121c41da598efa63166b533853a54f297
Anton Drobyshev <antondrob@bk.ru>
2021-08-15 15:51:08 +0300

    sprint-2/step-2/attempt-2

--- src/components/burger-ingredient/BurgerIngredient.js
+++ src/components/burger-ingredient/BurgerIngredient.js
@@ -5,6 +5,7 @@ import React from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {useDrag} from "react-dnd";
 import PropTypes from "prop-types";
+import {igredientPropTypes} from "../../prop-types";
 
 const BurgerIngredient = ({item}) => {
     const dispatch = useDispatch();
@@ -40,20 +41,7 @@ const BurgerIngredient = ({item}) => {
 }
 
 BurgerIngredient.propTypes = {
-    item: PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        type: PropTypes.string.isRequired,
-        proteins: PropTypes.number.isRequired,
-        fat: PropTypes.number.isRequired,
-        carbohydrates: PropTypes.number.isRequired,
-        calories: PropTypes.number.isRequired,
-        price: PropTypes.number.isRequired,
-        image: PropTypes.string.isRequired,
-        image_mobile: PropTypes.string.isRequired,
-        image_large: PropTypes.string.isRequired,
-        __v: PropTypes.number.isRequired
-    }).isRequired
+    item: PropTypes.shape(igredientPropTypes).isRequired
 };
 
 export default BurgerIngredient;
